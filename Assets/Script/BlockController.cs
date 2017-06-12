70cebd4d505ebf63d18b21066d26dabc31fcb700
Kwang Bin <klee166@jhu.edu>
2017-06-12 16:33:40 -0400

    Let's begin set up unity and model

---
+++ Assets/Script/BlockController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+
+    private void Start()
+    {
+        transform.position = new Vector3(0, 3, 0); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            transform.Translate(0, 0, 1.600001f);
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            transform.Translate(0, 0, -1.600001f);
+        }
+
+
+        if (Input.GetKeyDown("right"))
+        {
+            transform.Translate(3.200001f, 0, 0);
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            transform.Translate(-3.200001f, 0, 0);
+        }
+    }
+    
+}
---
+++ Assets/Script/GeneralLandscape.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralLandscape : MonoBehaviour {
+
+    public int width = 10;
+    public int depth = 10;
+
+    public GameObject block;
+    private BoxCollider b_collider;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int z = 0; z < depth; z++)
+        {
+            for (int x = 0; x < width; x++) {
+
+            float width_box = x * 3.200001f / 2.0f;
+            float height_box = z * 1.600001f * 2.0f;
+            int y = 0;
+            Vector3 blockPos = new Vector3(height_box, y, width_box);
+            Instantiate(block, blockPos, Quaternion.identity);
+        }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
